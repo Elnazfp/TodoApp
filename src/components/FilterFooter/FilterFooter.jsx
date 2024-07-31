4fcaa025ea4b93867431365c289280f07721e1cd
Elnazfp <elnaz.fp81@gmail.com>
2024-08-01 00:15:15 +0330

    Initial commit

---
+++ src/components/FilterFooter/FilterFooter.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const FilterFooter = () => (
+  <div className="FilterFooter">
+   <div>2 items</div>
+        <div>
+          <ul>
+            <li>
+              <button>All</button>
+            </li>
+            <li>
+              <button>Active</button>
+            </li>
+            <li>
+              <button>Completed</button>
+            </li>
+          </ul>
+        </div>
+  </div>
+);
+
+export default FilterFooter;
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => (
+    <footer>Developed by @ElnazFarjad</footer>
+);
+
+export default Footer;
